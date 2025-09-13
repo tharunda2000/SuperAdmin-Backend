ba6b7e4550d8c8377ff7e7c5002fbfedf0610c32
Tharunda De Silva <tharundadesilva@gmail.com>
2025-09-13 13:22:18 +0530

    first commit

---
+++ superAdmin/superAdmin/src/main/java/com/example/superAdmin/SuperAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.superAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAdminApplication.class, args);
+	}
+
+}
---
+++ superAdmin/superAdmin/src/test/java/com/example/superAdmin/SuperAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
