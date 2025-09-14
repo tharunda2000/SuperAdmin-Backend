2b6f06df0808680074c52905d0063cf26bac44a9
Tharunda De Silva <tharundadesilva@gmail.com>
2025-09-14 22:55:25 +0530

    RepositoryImpl - addDatabase method completed

---
+++ superAdmin/superAdmin/src/main/java/com/example/superAdmin/RepositoryImpl/DatabaseRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.example.superAdmin.RepositoryImpl;
+
+import com.example.superAdmin.Model.Database;
+import com.example.superAdmin.Repository.DatabaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Repository
+public class DatabaseRepositoryImpl implements DatabaseRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void addDatabase(Database database){
+
+        String insertQuery = "INSERT INTO databases (host,port,database_name,username,password,created_at,updated_at)"+
+                "VALUES (?,?,?,?,?,?,?)";
+
+        jdbcTemplate.update(insertQuery,
+                database.getHost(),
+                database.getPort(),
+                database.getDatabaseName(),
+                database.getUsername(),
+                database.getPassword(),
+                new Timestamp(System.currentTimeMillis()),
+                new Timestamp(System.currentTimeMillis())
+        );
+    }
+
+    List<Database> getAllDatabases();
+    Database getDatabaseById(int id);
+    void updateDatabase(Database database);
+    void deleteDatabaseById(int id);
+
+}
